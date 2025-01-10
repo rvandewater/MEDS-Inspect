c43d397801e551b18d209b3773f71801a5362ee5
Robin Philippus van de Water <vander09@li04e02.chimera.hpc.mssm.edu>
2025-01-10 09:44:23 -0500

    small adjustments

--- src/meds-vis/app.py
+++ src/meds-vis/app.py
@@ -12,10 +12,11 @@ app = Dash()
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 # df = pl.scan_parquet("/Users/robin/Documents/datasets/meds/northwestern/data/train").select(pl.col("time").hist()).collect()
-file_path = "/Users/robin/Documents/datasets/meds/northwestern/data/train"
+file_path = "/sc/arion/projects/hpims-hpi/projects/foundation_models_ehr/cohorts/meds/full_omop_source_concept_25_1_6/data"
 # Create a lazyframe, convert the 'time' column to a datetime type, format it to year-month, convert to string, and calculate the histogram
 df = (
     pl.scan_parquet(file_path)
+    .filter((pl.col("time") >= pl.datetime(2000, 1, 1)) & (pl.col("time") <= pl.datetime(2025, 12, 31)))
     .with_columns(pl.col("time").dt.strftime("%Y-%m").cast(pl.String).alias("time_str"))
     .group_by("time_str")
     .agg(pl.count("time_str").alias("count"))
@@ -27,18 +28,21 @@ df = (
 #     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
 # }))
 
-fig = px.histogram(df, x="time_str", y="count")#bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+fig = px.histogram(df, x="time_str", y="count", nbins=len(df))
+
+#bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='MEDS INSPECT'),
 
     html.Div(children='''
-        Dash: A web application framework for your data.
+        Code count over the years
     '''),
 
     dcc.Graph(
         id='example-graph',
-        figure=fig
+        figure=fig,
+        style={'width': '90hh', 'height': '90vh'}
     )
 ])
 

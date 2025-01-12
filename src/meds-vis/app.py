8b2c5c318cc2e6cdde16dc166833f290a2d126c0
Robin van de Water <r.p.vandewater@gmail.com>
2025-01-12 18:19:05 +0100

    dramatically improved caching and user feedback

--- src/meds-vis/app.py
+++ src/meds-vis/app.py
@@ -1,24 +1,54 @@
+import logging
 import os
 from pathlib import Path
+import asyncio
 
 import numpy as np
 import polars as pl
-from dash import Dash, html, dcc, Input, Output, State
+from dash import Dash, html, dcc, Input, Output, State, callback_context
 import plotly.express as px
 from cache_results import cache_results
 from code_search import load_code_metadata, search_codes
 
+# Global variable to store cached results
+cached_results = None
 
-def run_app(file_path):
+
+def get_folder_size(path):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size
+
+
+def is_valid_path(path):
+    if path is None or path == "":
+        return False
+    if not os.path.isdir(path):
+        return False
+
+    data_dir = Path(path) / "data"
+    metadata_dir = Path(path) / "metadata"
+
+    if not data_dir.is_dir() or not metadata_dir.is_dir():
+        return False
+
+    data_files = list(data_dir.rglob("*.parquet"))
+    metadata_files = list(metadata_dir.rglob("*.parquet"))
+
+    return len(data_files) > 0 and len(metadata_files) > 0
+
+
+def run_app(file_path=None):
+    global cached_results
+
+    if file_path and is_valid_path(file_path):
+        cached_results = cache_results(file_path)
+
     app = Dash(__name__, suppress_callback_exceptions=True)
     app.title = "MEDS INSPECT"
-
-    code_count_years, code_count_subject, top_codes = cache_results(file_path)
-    code_metadata = load_code_metadata(file_path + "metadata/codes.parquet")
-
-    # Get unique patient IDs and codes
-    patient_ids = code_count_subject['subject_id'].unique().to_list()
-    codes = top_codes['code'].unique().to_list()
 
     app.layout = html.Div(children=[
         html.Div(
@@ -29,14 +59,32 @@ def run_app(file_path):
         ),
         html.H1(children='MEDS INSPECT 🔎', style={'textAlign': 'center'}),
         html.P(children='Explore and visualize your Medical Event Data Standard (MEDS) data.',
-               style={'textAlign': 'center'}),
+               style={'textAlign': 'center', 'fontSize': '20px'}),
+        dcc.Input(id='hidden-file-path', type='hidden', value=file_path,
+                  style={"textAlign": "center", "fontSize": "20px"}),
+        html.Div(id='path-feedback', style={'marginTop': '10px', 'color': 'green', 'textAlign': 'center'}),
+        html.Div([
+            dcc.Input(id='input-path', type='text', placeholder='Enter folder path',
+                      style={'width': '80%', 'margin': '0 auto', 'display': 'block', 'fontSize': '20px'}),
+            html.Button('Inspect 🔎', id='submit-path', n_clicks=0,
+                        style={'display': 'block', 'margin': '20px auto', 'fontSize': '20px'}),
+            dcc.Loading(
+                id='loading-feedback',
+                type='circle',
+                children=[
+                    html.Div(id='loading-output')
+                ],
+                overlay_style={"visibility": "visible", "filter": "blur(2px)"},
+            )
+        ], style={'marginTop': '20px'}),
         dcc.Tabs(id='tabs', value='tab-1', children=[
-            dcc.Tab(label='📅 Code count over time', value='tab-1'),
-            dcc.Tab(label='👤 Code count per patient', value='tab-2'),
-            dcc.Tab(label='🏆 Top most frequent codes', value='tab-3'),
-            dcc.Tab(label='🕒 Codes over time for a single patient', value='tab-4'),
-            dcc.Tab(label='📊 Numerical distribution for a single code', value='tab-5'),
+            dcc.Tab(label='📅 Yearly Overview', value='tab-1'),
+            dcc.Tab(label='👤 Per Patient Codes', value='tab-2'),
+            dcc.Tab(label='🏆 Top Codes', value='tab-3'),
+            dcc.Tab(label='🕒 Patient Timeline', value='tab-4'),
+            dcc.Tab(label='📊 Code Distribution', value='tab-5'),
             dcc.Tab(label='🔍 Code Search', value='tab-6'),
+            dcc.Tab(label='📖 Coding Dictionary', value='tab-7')
         ]),
         dcc.Loading(
             id='loading-tabs-content',
@@ -46,10 +94,44 @@ def run_app(file_path):
     ], style={'fontFamily': 'Helvetica', 'marginLeft': '30px', 'marginRight': '30px'})
 
     @app.callback(
-        Output('tabs-content', 'children'),
-        Input('tabs', 'value')
+        Output('hidden-file-path', 'value'),
+        Output('path-feedback', 'children'),
+        Output('loading-output', 'children'),
+        Input('submit-path', 'n_clicks'),
+        State('input-path', 'value'),
+        State('hidden-file-path', 'value')
     )
-    def render_content(tab):
+    def update_hidden_path(n_clicks, input_path, current_path):
+        global cached_results
+
+        if n_clicks == 0:
+            return current_path, ('Enter the path to your MEDS data folder to get started. '
+                                  'The first run will run several queries on the dataset; '
+                                  'this might take a while depending on the dataset size.'), ''
+        if n_clicks > 0 and is_valid_path(input_path):
+            folder_size = get_folder_size(input_path)
+            size_in_mb = folder_size / (1024 * 1024)
+            print(f"loading cached results at: {input_path}")
+            cached_results = cache_results(input_path)
+            feedback_message = f'Selected folder: {input_path} (Size: {size_in_mb:.2f} MB). Caching complete.'
+            return input_path, feedback_message, ''
+        return current_path, 'Invalid folder path. Please try again.', ''
+
+    @app.callback(
+        Output('tabs-content', 'children'),
+        Input('tabs', 'value'),
+        State('hidden-file-path', 'value')
+    )
+    def render_content(tab, file_path):
+        if not file_path:
+            return html.Div('No folder selected. Please enter a valid folder path to proceed.')
+
+        code_count_years, code_count_subject, top_codes, coding_dict = cached_results
+
+        # Get unique patient IDs and codes
+        patient_ids = code_count_subject['subject_id'].unique().to_list()
+        codes = top_codes['code'].unique().to_list()
+
         content_style = {'border': '2px solid #007BFF', 'padding': '10px', 'borderRadius': '5px'}
 
         if tab == 'tab-1':
@@ -67,7 +149,9 @@ def run_app(file_path):
                 )
             ], style=content_style)
         elif tab == 'tab-2':
-            fig_code_count_subject = px.histogram(code_count_subject, y="subject_id", x="count")
+            fig_code_count_subject = px.histogram(code_count_subject, y="subject_id", x="count", histfunc="count",
+                                                  title="Code count distribution per patient",
+                                                  labels={"count": "Code count", "count": "Amount of patients"})
             return html.Div([
                 html.H2(children='Code count per patient'),
                 dcc.Loading(
@@ -76,7 +160,7 @@ def run_app(file_path):
                     children=dcc.Graph(
                         id='fig_code_count_subject',
                         figure=fig_code_count_subject,
-                        style={'width': '90hh', 'height': '50vh'}
+                        style={'width': '90hh', 'height': '90vh'}
                     )
                 )
             ], style=content_style)
@@ -88,9 +172,7 @@ def run_app(file_path):
                     options=[
                         {'label': 'Top 10', 'value': 10},
                         {'label': 'Top 50', 'value': 50},
-                        {'label': 'Top 100', 'value': 100},
-                        {'label': 'Top 250', 'value': 250},
-                        {'label': 'Top 500', 'value': 500}
+                        {'label': 'Top 100', 'value': 100}
                     ],
                     value=100,
                     placeholder='Select top N codes'
@@ -147,7 +229,7 @@ def run_app(file_path):
                     options=[
                         {'label': 'Code', 'value': 'code'},
                         {'label': 'Description', 'value': 'description'},
-                        {'label': 'Parent Code', 'value': 'parent_codes'}
+                        {'label': 'Parent Codes', 'value': 'parent_codes'}
                     ],
                     value=['code', 'description', 'parent_codes'],
                     multi=True,
@@ -160,32 +242,48 @@ def run_app(file_path):
                     children=html.Div(id='search-results')
                 )
             ], style=content_style)
+        elif tab == 'tab-7':
+            fig_coding_dict = px.bar(coding_dict, x="coding_dict", y="count", title="Coding Dictionary Overview",
+                                     color="coding_dict")
+            return html.Div([
+                html.H2(children='Coding Dictionary Overview'),
+                dcc.Loading(
+                    id='loading-fig-coding-dict',
+                    type='default',
+                    children=dcc.Graph(
+                        id='fig_coding_dict',
+                        figure=fig_coding_dict,
+                        style={'width': '90hh', 'height': '90vh'}
+                    )
+                )
+            ], style=content_style)
 
     @app.callback(
         Output('search-results', 'children'),
         Input('search-button', 'n_clicks'),
         Input('search-term', 'n_submit'),
         State('search-term', 'value'),
-        State('search-options', 'value')
+        State('search-options', 'value'),
+        State('hidden-file-path', 'value')
     )
-    def update_search_results(n_clicks, n_submit, search_term, search_options):
+    def update_search_results(n_clicks, n_submit, search_term, search_options, file_path):
         if (n_clicks is None and n_submit == 0) or not search_term:
             return "Enter a search term to find codes."
 
+        code_metadata = load_code_metadata(file_path + "/metadata/codes.parquet")
         results = search_codes(code_metadata, search_term, search_options)
         if len(results) == 0:
             return "No results found."
 
-
         return [html.Tbody([f"Found {len(results)} results" if len(results) < 1000
                             else "Showing first 1000 results found. Please refine search"]),
-html.Table([
-            html.Thead(html.Tr([html.Th(col) for col in results.columns])),
-            html.Tbody([
-                html.Tr([html.Td(results[col].to_list()[i]) for col in results.columns])
-                for i in range(len(results))
-            ])
-        ])]
+                html.Table([
+                    html.Thead(html.Tr([html.Th(col) for col in results.columns])),
+                    html.Tbody([
+                        html.Tr([html.Td(results[col].to_list()[i]) for col in results.columns])
+                        for i in range(len(results))
+                    ])
+                ])]
 
     @app.callback(
         Output('fig_top_codes', 'figure'),
@@ -270,5 +368,7 @@ html.Table([
 
 # Example usage
 if __name__ == '__main__':
-    file_path = "/Users/robin/Documents/datasets/meds/northwestern/"
+    logging.format = '%(asctime)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+    file_path = "/Users/robin/Documents/datasets/meds/northwestern/"  # Set to None to prompt folder selection
     run_app(file_path)
--- src/meds-vis/cache_results.py
+++ src/meds-vis/cache_results.py
@@ -1,52 +1,64 @@
-import os
+import logging
 from pathlib import Path
-
 import polars as pl
 
 def cache_results(file_path):
-    cache_dir = Path(file_path) / "cache"
+    cache_dir = Path(file_path) / ".meds_inspect_cache"
+    logging.info(f"Running cache_results on {file_path}")
+
+    # Check if all cached files exist
+    code_count_years_path = cache_dir / "code_count_years.parquet"
+    code_count_subjects_path = cache_dir / "code_count_subjects.parquet"
+    top_codes_path = cache_dir / "top_codes.parquet"
+    coding_dict_path = cache_dir / "coding_dict.parquet"
+
+    if (code_count_years_path.exists() and
+        code_count_subjects_path.exists() and
+        top_codes_path.exists() and
+        coding_dict_path.exists()):
+        # Load the cached results
+        code_count_years = pl.read_parquet(code_count_years_path)
+        code_count_subjects = pl.read_parquet(code_count_subjects_path)
+        top_codes = pl.read_parquet(top_codes_path)
+        coding_dict = pl.read_parquet(coding_dict_path)
+        logging.info(f"Loaded cached results at {cache_dir}")
+        return code_count_years, code_count_subjects, top_codes, coding_dict
 
     print("file_path", file_path)
-    data = pl.scan_parquet(Path(file_path) / "data/*/*.parquet" )
+    data = pl.scan_parquet(Path(file_path) / "data/*/*.parquet")
     print("Columns in the file:", data.collect_schema().names())
     # Create the cache directory if it does not exist
     cache_dir.mkdir(parents=True, exist_ok=True)
 
-    code_count_years_path = cache_dir / "code_count_years.parquet"
-    if code_count_years_path.exists():
-        # Load the cached results
-        code_count_years = pl.read_parquet(code_count_years_path)
-    else:
+    # Initialize variables
+    code_count_years = None
+    code_count_subjects = None
+    top_codes = None
+    coding_dict = None
+
+    if not code_count_years_path.exists():
         # Compute the results and save to cache
         code_count_years = (
             data
-            # .select(pl.col("time"))
             .filter((pl.col("time") >= pl.datetime(2000, 1, 1)) & (pl.col("time") <= pl.datetime(2025, 12, 31)))
             .with_columns(pl.col("time").dt.strftime("%Y-%m").cast(pl.String).alias("time_str"))
             .group_by("time_str")
             .agg(pl.count("time_str").alias("count"))
-            # .collect()
         )
         code_count_years.sink_parquet(code_count_years_path)
 
-    code_count_subjects_path = cache_dir / "code_count_subjects.parquet"
-    if code_count_subjects_path.exists():
-        # Load the cached results
-        code_count_subjects = pl.read_parquet(code_count_subjects_path)
-    else:
+    if not code_count_subjects_path.exists():
         # Compute the results and save to cache
-        code_count_subjects = (data
+        code_count_subjects = (
+            data
             .select(pl.col("subject_id"), pl.col("code"))
             .group_by("subject_id")
             .agg(pl.count("code").alias("count"))
             .collect()
         )
         code_count_subjects.write_parquet(code_count_subjects_path)
-    top_codes_path = cache_dir / f"top_codes.parquet"
-    if top_codes_path.exists():
-        # Load the cached results
-        top_codes = pl.read_parquet(top_codes_path)
-    else:
+
+    if not top_codes_path.exists():
         # Compute the results and save to cache
         top_codes = (
             data
@@ -55,4 +67,28 @@ def cache_results(file_path):
             .sort("count", descending=True)
             .collect()
         )
-    return code_count_years, code_count_subjects, top_codes
+        top_codes.write_parquet(top_codes_path)
+
+    if not coding_dict_path.exists():
+        # Compute the results and save to cache
+        coding_dict = (
+            data
+            .with_columns(pl.col("code").str.split("//").list.first().alias("coding_dict"))
+            .group_by("coding_dict")
+            .agg(pl.count("coding_dict").alias("count"))
+            .collect()
+        )
+        coding_dict.write_parquet(coding_dict_path)
+
+    # Load the results if they were not loaded from cache
+    if code_count_years is None:
+        code_count_years = pl.read_parquet(code_count_years_path)
+    if code_count_subjects is None:
+        code_count_subjects = pl.read_parquet(code_count_subjects_path)
+    if top_codes is None:
+        top_codes = pl.read_parquet(top_codes_path)
+    if coding_dict is None:
+        coding_dict = pl.read_parquet(coding_dict_path)
+
+    logging.info(f"Saved cache to: {cache_dir}")
+    return code_count_years, code_count_subjects, top_codes, coding_dict

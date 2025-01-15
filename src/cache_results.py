f63827dad86e2247d435e1fd041ef248ac6df1aa
Robin van de Water <r.p.vandewater@gmail.com>
2025-01-15 13:10:23 +0100

    argument

--- src/cache_results.py
+++ src/cache_results.py
@@ -146,7 +146,7 @@ def load_generated_cache(cache_dir, cache_files):
 
 def main():
     parser = argparse.ArgumentParser(description='Run cache_results with a specified file path.')
-    parser.add_argument('file_path', type=str, help='The path to the MEDS data folder')
+    parser.add_argument('--file_path', type=str, help='The path to the MEDS data folder')
     parser.add_argument('--invalidate',action='store_true', help='Invalidate the cache')
     args = parser.parse_args()
 
